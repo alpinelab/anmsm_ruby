137f2a8e4a8532b8438a114cdf3b1187880fb9b0
Lucas BIGUET-MERMET <lucas.biguetmermet@gmail.com>
2013-08-16 17:22:44 +0200

    create sw_resort from anmsm_resort and format resort name

--- anmsm.rb
+++ anmsm.rb
@@ -1,5 +1,6 @@
 # coding: utf-8
 require 'ruby_odata'
+require 'i18n'
 
 ##### Gem
 
@@ -40,19 +41,78 @@ end
 
 class AnmsmSync
 
+  # def generate_resort_slug(resort_name)
+  #   return nil if resort_name.nil? || resort_name.match(/^\s*$/)
+  #   slug = resort_name.clone
+  #   #apostrophes
+  #   slug.gsub!(/'/, '-') if slug.match(/'/)
+  #   # remplacer les "/" par "-"    
+  #   slug.gsub!(/\//, '-') if slug.match(/\//)
+  #   #gerer les tirets-espace 
+  #   slug.gsub!(/( *- *)/,'-') if slug.match(/( *- *)/)    
+  #   # remplacer " " par "-"
+  #   slug.gsub!(/ +/, '-') if slug.match(/ +/)   
+  #   # minuscule
+  #   I18n.transliterate(slug.downcase)
+  # end
+
+  def generate_resort_name(resort_name)
+    return nil if resort_name.nil? || resort_name.match(/^\s*$/)
+    name = resort_name.clone
+    #virer les espaces superflus
+    name.gsub!(/ {2,}/, ' ') if name.match(/ {2,}/)
+    name.gsub!(/^ | $/, '') if name.match(/^ | $/)
+    #gerer les slash
+    name.gsub!(/(\b *\/ *\b)/, ' / ') if name.match(/(\b *\/ *\b)/)
+    #convertir les " - " en " / "
+    name.gsub!(/\b - \b/, ' / ') if name.match(/\b - \b/)
+    name.gsub!(/(\b- \b)|(\b -\b)/, '-') if name.match(/(\b- \b)|(\b -\b)/)
+    #capitaliser chaque mot tout en gerant les mots composés (ex: Crest-volant) et les apostrophes (ex: Alpes D'huez)    
+    words = name.split(/( |'|-)/)
+    words.each do |i|
+      i.downcase!
+      i.capitalize! unless i == nil || i.length <= 1
+    end
+    name = words.join("")
+  end
+
   def sync_weather_for_all_resorts
     anmsm = AnmsmGem.new()
     resorts = anmsm.all_resorts
     puts "#{resorts.length} stations récupérées."
-    resorts.each do |resort|
+    resorts.each do |resort| 
+      slug = generate_resort_slug(resort.NOMSTATION) 
+      display_name = generate_resort_name(resort.NOMSTATION)    
       weather = anmsm.weather_for_resort(resort)
       if weather.length == 1
-        update_weather_for_resort(weather[0], resort.NOMSTATION)
+        #update_weather_for_resort(weather[0], resort.NOMSTATION)
+        puts "météo ok avec la station #{display_name} (slug: '#{slug}')"
       else
-        puts "météo ko avec la station #{resort.NOMSTATION}"
+        puts "météo ko avec la station #{display_name} (slug: '#{slug}')"
       end
     end
   end
+
+  def sync_infos_for_all_resorts
+    anmsm = AnmsmGem.new()
+    resorts = anmsm.all_resorts
+    puts "#{resorts.length} stations récupérées."
+    resorts.each do |resort| 
+      create_sw_resort_from_anmsm_resort(resort)
+    end
+  end
+
+  def create_sw_resort_from_anmsm_resort(anmsm_resort)
+    sw_resort = Resort.new
+    sw_resort.name = generate_resort_name(anmsm_resort.NOMSTATION)
+    sw_resort.save!
+  end
+
+  def create_resort_infos(resort)
+    
+  end
+
+  #TODO gerer les cas nordique / alpin (cf Autrans) pour retrouver le slug
 
   def update_weather_for_resort(weather, resort_name)
     resort = Resort.find_by_slug(Resort.slug_me_please(resort_name))
@@ -68,8 +128,7 @@ class AnmsmSync
       rwi.weather_afternoon = weather.TEMPAPMHIVER,
       rwi.weather_morning = weather.TEMPAPMHIVER
   end
-
 end
 
-sync = AnmsmSync.new()
-sync.sync_weather_for_all_resorts
+#sync = AnmsmSync.new()
+#sync.sync_weather_for_all_resorts
