2cc8bdfc2667ff0884f9911045bafc5e3016da1a
Lucas BIGUET-MERMET <lucas.biguetmermet@gmail.com>
2013-08-23 12:10:04 +0200

    rename some methods, simplify tests

--- lib/anmsm_ruby.rb
+++ lib/anmsm_ruby.rb
@@ -3,6 +3,7 @@ require 'ruby_odata'
 
 module AnmsmRuby
   class Base
+    attr_accessor :resort_svc
     def initialize
       # Resort feed
       @resort_svc = OData::Service.new "http://wcf.tourinsoft.com/Syndication/anmsm/817bee9d-faf4-4680-9d05-e41c2c90ae5a", {:namespace => "Anmsm::Resort"}
@@ -12,21 +13,25 @@ module AnmsmRuby
       @snow_svc = OData::Service.new "http://wcf.tourinsoft.com/Syndication/anmsm/ce4e4296-6132-4e11-b8db-ea21b92e736c", {:namespace => "Anmsm::Snow"}
     end
 
-    def resort_snow_infos(resort_anmsm_id)
-      @snow_svc.Objects.filter("STATION eq '#{resort_anmsm_id}'").order_by("Updated")
-      @snow_svc.execute
-    end
-
+    #returns general info for all resorts
     def all_resorts
       @resort_svc.Objects.order_by("NOMSTATION")
       @resort_svc.execute
     end
 
-    def resort_by_name(resort_name)
+    #returns general info for the given resorts
+    def info_for_resort(resort_name)
       @resort_svc.Objects.filter("NOMSTATION eq '#{resort_name}'")
       @resort_svc.execute
     end
 
+    #returns snow info for the given resort
+    def snow_info_for_resort(resort_anmsm_id)
+      @snow_svc.Objects.filter("STATION eq '#{resort_anmsm_id}'").order_by("Updated")
+      @snow_svc.execute
+    end
+
+    #returns weather info for the given resort
     def weather_for_resort(resort, date = Time.now)
       resort_id = resort.SyndicObjectID
       @weather_svc.Objects.filter("STATION eq '#{resort_id}'").filter("day(Updated)  eq #{date.day}")
--- spec/anmsm_spec.rb
+++ spec/anmsm_spec.rb
@@ -1,19 +1,22 @@
 require 'spec_helper'
 
-describe 'initialize' do
-  before :each do
-    @instance = AnmsmRuby::Base.new
-  end
+describe AnmsmRuby::Base do
 
-  it 'should assign @resort_svc' do
-    @instance.instance_variable_get(:@resort_svc).should_not be_nil
-  end
+  describe "#initialize" do
+    before :all do
+      @instance = AnmsmRuby::Base.new
+    end
 
-  it 'should assign @weather_svc' do
-    @instance.instance_variable_get(:@weather_svc).should_not be_nil
-  end
+    it 'should assign @resort_svc' do
+      @instance.instance_variable_get(:@resort_svc).should be_an_instance_of OData::Service
+    end
 
-  it 'should assign @snow_svc' do
-    @instance.instance_variable_get(:@snow_svc).should_not be_nil
+    it 'should assign @weather_svc' do
+      @instance.instance_variable_get(:@weather_svc).should be_an_instance_of OData::Service
+    end
+
+    it 'should assign @snow_svc' do
+      @instance.instance_variable_get(:@snow_svc).should be_an_instance_of OData::Service
+    end
   end
 end
